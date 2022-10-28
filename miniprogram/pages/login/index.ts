b788fff4b3dca9049f08c0330ae6f447e8ee1df8
xianhua347 <3389162133@qq.com>
2022-10-28 21:42:57 +0800

    feat(root):add login page

---
+++ miniprogram/pages/login/index.ts
@@ -0,0 +1,23 @@
+// pages/login/index.ts
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
